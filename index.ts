31f4272bcd19deef0f96f06a555eeb68802498c8
huslee <Xvclee2551@gmail.com>
2023-04-06 23:45:29 +0800

    map

--- index.ts
+++ index.ts
@@ -2,30 +2,41 @@ import axios from 'axios';
 import express, { Express, Request, Response } from 'express';
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Dian:dzj0jRzEofkDNyZb@cluster0.m5m6cex.mongodb.net/project').then(() => console.log('MongoDB Connected ✅'));
+mongoose.connect('mongodb+srv://Dian:Dian2022@cluster0.m5m6cex.mongodb.net/project').then(() => console.log('MongoDB Connected ✅'));
 
 const app: Express = express();
 const port = 8000;
 
+let endpoints = [
+  `https://api.hitech.mn/api/categories/6ac583ed-76c9-419a-9b2f-df3290cf6bc1`,
+  `https://api.hitech.mn/api/categories/b4a6f30a-6f30-44d4-8e3c-922547361857`,
+  `https://api.hitech.mn/api/categories/642a9ec6-668e-46f7-9701-5b2ef9dc3e36`,
+  `https://api.hitech.mn/api/categories/dc222884-ecc3-4b9a-ace9-e9a863129df7`,
+  `https://api.hitech.mn/api/categories/a81c5bd2-4820-4801-93c7-b85dfc89cab9`,
+  `https://api.hitech.mn/api/categories/45fe6a0e-33db-4e40-8a4f-e789e86ff63d`,
+]
 
 
 app.get("/data",(req,res)=>{
-  axios.get(`https://api.hitech.mn/api/menu/category${}`)
-  .then(async function (response) {
-    console.log(response.data[0].sub_menu[0]);
-    // const categorySchema = new mongoose.Schema({}, { strict: false })
-    //     const TestCollection = mongoose.model('category', categorySchema)
-    //     let body = response?.data
-    //     const testCollectionData = new TestCollection(body)
-    //     await testCollectionData.save()
-        return res.send(
-            response.data
-        )
-    
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  axios.all(endpoints.map((endpoints)=>
+    axios.get(endpoints)
+    .then(async function (response) {
+      console.log(response);
+      // const categorySchema = new mongoose.Schema({}, { strict: false })
+      //     const TestCollection = mongoose.model('category', categorySchema)
+      //     let body = response
+      //     const testCollectionData = new TestCollection(body)
+      //     await testCollectionData.save()
+          return res.send(
+              response
+          )
+      
+    } )
+    .catch(function (error) {
+      console.log(error);
+    })
+  ))
+  
 })
 
 
