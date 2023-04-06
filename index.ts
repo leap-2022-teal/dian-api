309f298c1a068819821aba07c4a85213a757eb1a
oyuerdene <teal.oyuerdene@gmail.com>
2023-04-06 18:51:24 +0800

    insert data

--- index.ts
+++ index.ts
@@ -1,27 +1,33 @@
+import axios from 'axios';
 import express, { Express, Request, Response } from 'express';
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Dian:cf59aa39958d@cluster0.m5m6cex.mongodb.net/project').then(() => console.log('MongoDB Connected ✅'));
+mongoose.connect('mongodb+srv://Dian:dzj0jRzEofkDNyZb@cluster0.m5m6cex.mongodb.net/project').then(() => console.log('MongoDB Connected ✅'));
 
 const app: Express = express();
-const port = 8080;
+const port = 3535;
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  createdAt: Date,
-});
-const User = mongoose.model('User', userSchema);
 
-app.get('/test-mongoose', (req, res) => {
-  const user = User.create({
-    name: 'Bold',
-    age: 18,
-    createdAt: new Date(),
+
+app.get("/data",(req,res)=>{
+  axios.get(`https://api.hitech.mn/api/products/?category=7b0e308a-1e0e-4761-a805-06c1845d5562&limit=16&offset=0`)
+  .then(async function (response) {
+    console.log(response.data);
+    // const categorySchema = new mongoose.Schema({}, { strict: false })
+    //     const TestCollection = mongoose.model('category', categorySchema)
+    //     let body = response?.data
+    //     const testCollectionData = new TestCollection(body)
+    //     await testCollectionData.save()
+        return res.send(
+            response.data?.data
+        )
+    
+  })
+  .catch(function (error) {
+    console.log(error);
   });
+})
 
-  res.json(user);
-});
 
 app.get('/', (req: Request, res: Response) => {
 res.send('Dian project');
