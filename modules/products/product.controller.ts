eac7c9f7e67bf943d4f323e2b3c0ec05b5d25dec
oyu <teal.2022.oyuerdene@gmail.com>
2023-04-13 18:11:48 +0800

    limit

--- modules/products/product.controller.ts
+++ modules/products/product.controller.ts
@@ -3,6 +3,6 @@ import { title } from 'process';
 import { Product } from './product.model';
 
 export async function getProduct(req: Request, res: Response) {
-  const list = await Product.find({});
+  const list = await Product.find({}, {}, { limit: 12 });
   res.json(list);
 }
