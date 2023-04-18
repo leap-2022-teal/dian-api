3211726eb9a51445d63ac203172c4ddcb53bb156
oyu <teal.2022.oyuerdene@gmail.com>
2023-04-19 01:25:57 +0800

    category update

--- modules/categories/category.routes.ts
+++ modules/categories/category.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createNewCategory, deleteCategoryById, getCategory } from './category.controller';
+import { createNewCategory, deleteCategoryById, getCategory, updateCategoryById } from './category.controller';
 
 const router = Router();
 
 router.get('/', getCategory);
 router.post('/', createNewCategory);
 router.delete('/:id', deleteCategoryById);
+router.put('/:id', updateCategoryById);
 
 export const categoriesRouter = router;
