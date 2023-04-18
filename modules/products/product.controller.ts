26f1a07a1c32c87d85458f74ae51ee7d7d0e4816
XvcleeD <Xvclee2551@gmail.com>
2023-04-18 19:32:11 +0800

    productCreate

--- modules/products/product.controller.ts
+++ modules/products/product.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
-import { title } from 'process';
+import { ObjectId } from 'mongodb';
 import { Product } from './product.model';
 
 export async function getProduct(req: Request, res: Response) {
   const list = await Product.find({}, {}, { limit: 12 });
   res.json(list);
+}
+
+export async function createNewProductd(req: Request, res: Response) {
+  const { title } = req.body;
+  const newProduct = new Product({
+    _id: new ObjectId(),
+    title: title,
+  })
+  const result = await newProduct.save();
+  res.sendStatus(200);
 }
