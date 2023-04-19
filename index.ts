4c09fc3ca38e3ad5b02f9ae56642c4716a31f3aa
oyu <teal.2022.oyuerdene@gmail.com>
2023-04-19 18:32:05 +0800

    user register post request initial

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, Request, Response } from 'express';
-import mongoose from "mongoose";
-import { categoriesRouter } from "./modules/categories/category.routes";
+import mongoose from 'mongoose';
+import { categoriesRouter } from './modules/categories/category.routes';
 import { productsRouter } from './modules/products/product.routes';
-
+import { usersRouter } from './modules/users/user.routes';
 
 dotenv.config();
 
@@ -20,9 +20,9 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Dian project');
 });
 
-app.use("/categories" , categoriesRouter)
+app.use('/categories', categoriesRouter);
 app.use('/products', productsRouter);
-
+app.use('/user', usersRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at ${port} 🎉`);
