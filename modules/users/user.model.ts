742e0d70a0a39ac4c0ab052278e7d3cf7bd66577
oyu <teal.2022.oyuerdene@gmail.com>
2023-05-17 18:00:15 +0800

    createdDate

--- modules/users/user.model.ts
+++ modules/users/user.model.ts
@@ -5,12 +5,14 @@ export interface IUser {
   email: String;
   password: String;
   role?: { type: string; enum: ['admin', 'user'] };
+  createdDate: Date;
 }
 
 const userSchema = new Schema<IUser>({
   email: { type: String, required: [true, 'Email хаягаа оруулна уу'] },
   password: { type: String, required: [true, 'Нууц үгээ оруулна уу'] },
   role: { type: String, enum: ['admin', 'client'], default: 'client' },
+  createdDate: { type: Date },
 });
 
 export const User = model<IUser>('users', userSchema);
